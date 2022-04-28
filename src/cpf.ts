13e1138b47d7dc39dc84b9a984ea8a257e39e947
Bryan Guillen <bryanguillenlds@gmail.com>
2022-04-27 23:17:09 -0600

    Started refactoring to turn the code more Object Oriented and also to be more Test-Driven

--- src/cpf.ts
+++ src/cpf.ts
@@ -1,40 +1,48 @@
-const FIRST_DIGIT_FACTOR = 10;
-const SECOND_DIGIT_FACTOR = 11;
+export default class Cpf {
+  readonly value: string;
+  private FIRST_DIGIT_FACTOR = 10;
+  private SECOND_DIGIT_FACTOR = 11;
 
-export function validateCpf (rawCpf: string | null | undefined) {
-	if (!rawCpf) return false;
-  const cpf = cleanCpf(rawCpf);
-  if (isInvalidLength(cpf)) return false;
-  if (isIdenticalDigits(cpf)) return false;
-  const calculatedCheckDigit1 = calculateCheckDigit(cpf, FIRST_DIGIT_FACTOR);
-  const calculatedCheckDigit2 = calculateCheckDigit(cpf, SECOND_DIGIT_FACTOR);
-  let checkDigit = extractCheckDigit(cpf);
-  const calculatedCheckDigit = `${calculatedCheckDigit1}${calculatedCheckDigit2}`;
-  return checkDigit === calculatedCheckDigit;
-}
-
-function cleanCpf(cpf: string) {
-  return cpf.replace(/\D/g,"");
-}
-
-function isInvalidLength(cpf: string) {
-  return cpf.length !== 11;
-}
-
-function isIdenticalDigits(cpf: string) {
-  const [firstDigit] = cpf;
-  return [...cpf].every(digit => digit === firstDigit);
-}
-
-function calculateCheckDigit(cpf: string, factor: number) {
-  let total = 0;
-  for (const digit of cpf) {
-    if (factor > 1) total += parseInt(digit) * factor--;
+  constructor(value: string) {
+    if (!this.validate(value)) throw new Error("CPF Invalido");
+    this.value = value;
   }
-  const rest = total%11;
-  return (rest < 2) ? 0 : 11 - rest;
-}
 
-function extractCheckDigit(cpf: string) {
-  return cpf.slice(9);
+  private validate (rawCpf: string | null | undefined) {
+    if (!rawCpf) return false;
+    const cpf = this.cleanCpf(rawCpf);
+    if (this.isInvalidLength(cpf)) return false;
+    if (this.isIdenticalDigits(cpf)) return false;
+    const calculatedCheckDigit1 = this.calculateCheckDigit(cpf, this.FIRST_DIGIT_FACTOR);
+    const calculatedCheckDigit2 = this.calculateCheckDigit(cpf, this.SECOND_DIGIT_FACTOR);
+    let checkDigit = this.extractCheckDigit(cpf);
+    const calculatedCheckDigit = `${calculatedCheckDigit1}${calculatedCheckDigit2}`;
+    return checkDigit === calculatedCheckDigit;
+  }
+
+  private cleanCpf(cpf: string) {
+    return cpf.replace(/\D/g,"");
+  }
+
+  private isInvalidLength(cpf: string) {
+    return cpf.length !== 11;
+  }
+
+  private isIdenticalDigits(cpf: string) {
+    const [firstDigit] = cpf;
+    return [...cpf].every(digit => digit === firstDigit);
+  }
+
+  private calculateCheckDigit(cpf: string, factor: number) {
+    let total = 0;
+    for (const digit of cpf) {
+      if (factor > 1) total += parseInt(digit) * factor--;
+    }
+    const rest = total%11;
+    return (rest < 2) ? 0 : 11 - rest;
+  }
+
+  private extractCheckDigit(cpf: string) {
+    return cpf.slice(9);
+  }
 }
