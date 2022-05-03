d46f283e1505fbc5471f6063d8593f7107d9fc27
Bryan Guillen <bryan.guillen.lds@gmail.com>
2022-05-02 22:38:06 -0600

    Created a class to handle placing an order and also classes for the order inputs and outputs. Created interfaces and classes for dealing with a repository in memory for the CRUD operations. Organized folders to align with the layers of the application: 1- Application layer, for the application of the business rules in the entities. 2- Domain Layer, containing the entities and their business rules.  3- Infrastructure layer, containing the interface of the repositories to perform the interaction with a fake memory implementation. Also organized the tests into unit and integration tests.

---
+++ test/integration/PlaceOrder.test.ts
@@ -0,0 +1,23 @@
+import CouponRepositoryMemory from "../../src/infra/repository/memory/CouponRepositoryMemory";
+import ItemRepositoryMemory from "../../src/infra/repository/memory/ItemRepositoryMemory";
+import Order from "../../src/domain/entity/Order";
+import OrderRepositoryMemory from "../../src/infra/repository/memory/OrderRepositoryMemory";
+import PlaceOrder from "../../src/application/usecase/PlaceOrder";
+
+test("Deve fazer um pedido", function() {
+  const itemRepository = new ItemRepositoryMemory();
+  const orderRepository = new OrderRepositoryMemory();
+  const couponRepository = new CouponRepositoryMemory();
+  const placeOrder = new PlaceOrder(itemRepository, orderRepository, couponRepository);
+  const placeOrderInput = {
+    cpf: "935.411.347-80",
+    orderItems: [
+      { idItem: 1, quantity: 1 },
+      { idItem: 2, quantity: 1 },
+      { idItem: 3, quantity: 3 }
+    ],
+    coupon: "VALE20"
+  }
+  const output = placeOrder.execute(placeOrderInput);
+  expect(output.total).toBe(4872);
+});
