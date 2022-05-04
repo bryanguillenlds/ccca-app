b5ea2c35ff795db86a03936c84c5225247a2dc1e
Bryan Guillen <bryan.guillen.lds@gmail.com>
2022-05-03 18:43:23 -0600

    Refactored app and tests to include a GetItem use-case in the application layer. Modified PlaceOrder class to use a typed DTO for input and output. Changed repository memory files to return promises and defined a couple of different methods in them. Also modified their parent interfaces.

--- src/application/usecase/PlaceOrderInput.ts
+++
@@ -1,8 +0,0 @@
-export default class PlaceOrderInput {
-
-  constructor(
-    readonly cpf: string,
-    readonly orderItems: { idItem: number, quantity: number }[],
-    readonly coupon?: string
-  ) {}
-}
