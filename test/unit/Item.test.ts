bb5b52ed066f5f7eb62cfab006b946a6fc244132
Bryan Guillen <bryan.guillen.lds@gmail.com>
2022-05-17 21:30:26 -0600

    Refactored to include tests and use case for GetOrder. Added controllers for item and order endpoints, and added factories for repositories (memory and database). Added a couple of new columns to DB.

--- test/unit/Item.test.ts
+++ test/unit/Item.test.ts
@@ -1,14 +1,6 @@
 import Dimension from "../../src/domain/entity/Dimension";
 import Item from "../../src/domain/entity/Item";
 
-test("Deve criar um item com dimensoes e calcular o volume", function() {
-  const item = new Item(1, "Guitarra", 1000, new Dimension(100, 30, 10));
-  const volume = item.getVolume();
-  expect(volume).toBe(0.03);
-});
-
-test("Deve criar um item com dimensoes e calcular densidade", function() {
-  const item = new Item(1, "Guitarra", 1000, new Dimension(100, 30, 10), 3);
-  const density = item.getDensity();
-  expect(density).toBe(100);
+test("Deve lançar uma exception se o peso for negativo", function () {
+	expect(() => new Item(1, "Guitarra", 1000, new Dimension(100, 30, 10), -3)).toThrow(new Error("Invalid weight"));
 });
